68cf55e9ba3ea6017c72654aaf4f024473b52922
GotEmma <emmafah@student.chalmers.se>
2015-10-06 11:31:28 +0200

    Created JsonInfoExtract

---
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/backend/JsonInfoExtract.java
@@ -0,0 +1,12 @@
+package se.chalmers.student.devit.resekompanjon.backend;
+
+/**
+ * Created by emmafahlen on 2015-10-06.
+ */
+public class JsonInfoExtract {
+
+    public JsonInfoExtract(){
+
+
+    }
+}
