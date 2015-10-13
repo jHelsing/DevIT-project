751954f0a2f9d68fecb3eaa94721c55808eb85c9
Amaarillo <Amar_kulaglic@hotmail.com>
2015-10-13 15:07:05 +0200

    Work on currentTrip has started

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/DetailedTripView.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/DetailedTripView.java
@@ -1,15 +1,23 @@
 package se.chalmers.student.devit.resekompanjon;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import se.chalmers.student.devit.resekompanjon.fragment.NavigationDrawerFragment;
 
 /**
- * @author  Jonathan
+ * @author  Jonathan. Revisited by Amar.
  * @version  0.2
  */
 public class DetailedTripView extends AppCompatActivity
@@ -27,7 +35,15 @@ public class DetailedTripView extends AppCompatActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.detailed_trip_layout);
+
+        if(isOnBus()){
+
+            setContentView(R.layout.detailed_trip_layout);
+
+            initBasicTripInfo();
+        } else{
+            
+        }
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -38,11 +54,27 @@ public class DetailedTripView extends AppCompatActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.detailed_trip_drawer_layout));
 
-        initBasicTripInfo();
     }
 
     private void initBasicTripInfo() {
         //TODO initialise all the views with the right content
+    }
+
+    /**
+     * This method checks if the user's phone is connected to a SSID that belongs to a bus.
+     * @return Returns true if the user is on a bus.
+     */
+    private boolean isOnBus(){
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connMgr.getActiveNetworkInfo();
+        if(netInfo != null && netInfo.isConnected()){
+            WifiManager wifiMngr = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+            WifiInfo info = wifiMngr.getConnectionInfo();
+            return info.getSSID().equals("eduroam");
+        } else{
+            return false;
+        }
     }
 
     /**
