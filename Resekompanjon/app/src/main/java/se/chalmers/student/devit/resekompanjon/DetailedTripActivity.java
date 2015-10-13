13e8fefc156cd45e1dbe264ded5754e81dec8767
Amaarillo <Amar_kulaglic@hotmail.com>
2015-10-13 19:05:15 +0200

    busNumber and busDestination is done in detailedTrip

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/DetailedTripActivity.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/DetailedTripActivity.java
@@ -9,9 +9,15 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
 
 import se.chalmers.student.devit.resekompanjon.backend.connectionBackend.ElectricityBackend;
 import se.chalmers.student.devit.resekompanjon.backend.connectionBackend.NoConnectionException;
@@ -25,7 +31,7 @@ import se.chalmers.student.devit.resekompanjon.fragment.NavigationDrawerFragment
 public class DetailedTripActivity extends AppCompatActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks, OnTaskCompleted{
 
-    ElectricityBackend eb = new ElectricityBackend(this, this);
+    ElectricityBackend eb;
 
     private JsonObject trip;
 
@@ -40,23 +46,7 @@ public class DetailedTripActivity extends AppCompatActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(isOnBus()){
-
-            setContentView(R.layout.detailed_trip_layout);
-            try{
-                eb.getInformation();
-            }
-            catch(NoConnectionException e){
-                Toast noConectionMessage = Toast.makeText(this
-                        , "OBS! Internetanslutning krävs!", Toast.LENGTH_LONG);
-                noConectionMessage.show();
-            }
-
-        } else{
-
-            setContentView(R.layout.detailed_trip_warning_layout);
-
-        }
+        setContentView(R.layout.detailed_trip_layout);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -67,6 +57,25 @@ public class DetailedTripActivity extends AppCompatActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.detailed_trip_drawer_layout));
 
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        if(isOnBus()){;
+            try{
+                eb = new ElectricityBackend(this, this);
+                eb.getInformation();
+            }
+            catch(NoConnectionException e){
+                Toast noConectionMessage = Toast.makeText(this
+                        , "OBS! Internetanslutning krävs!", Toast.LENGTH_LONG);
+                noConectionMessage.show();
+            }
+        } else{
+            setContentView(R.layout.detailed_trip_warning_layout);
+        }
     }
 
     private void initBasicTripInfo() {
@@ -116,8 +125,33 @@ public class DetailedTripActivity extends AppCompatActivity
 
     @Override
     public void onTaskCompleted() {
-        if(){
-            JsonObject js = eb.getApiData().getAsJsonObject();
+        JsonArray jsArray = eb.getApiData();
+        boolean condition = true;
+        JsonObject jsObj = null;
+        int i = 0;
+        while(condition){
+            jsObj = jsArray.get(i).getAsJsonObject();
+            if(jsObj.get("resourceSpec").getAsString().equals("Destination_Value")){
+                condition = false;
+            }
+            else{
+                i++;
+            }
         }
+        TextView busNmrTextview = (TextView) findViewById(R.id.busNumber);
+        busNmrTextview.setText(jsObj.get("value").getAsString());
+        condition = true;
+        i = 0;
+        while(condition){
+            jsObj = jsArray.get(i).getAsJsonObject();
+            if(jsObj.get("resourceSpec").getAsString().equals("Journey_Name_Value")){
+                condition = false;
+            }
+            else{
+                i++;
+            }
+        }
+        TextView busDirectionTextview = (TextView) findViewById(R.id.busDirection);
+        busDirectionTextview.setText(jsObj.get("value").getAsString());
     }
 }
