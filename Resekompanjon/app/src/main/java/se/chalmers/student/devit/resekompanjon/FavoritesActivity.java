040b335bc00e90bd5b63396bf0ecf06e8719b270
Helsing <jonathan.helsing@gmail.com>
2015-10-14 08:48:55 +0200

    Added nav and creation of list items to FavoritesAcitivty

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/FavoritesActivity.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/FavoritesActivity.java
@@ -1,13 +1,33 @@
 package se.chalmers.student.devit.resekompanjon;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+
+import se.chalmers.student.devit.resekompanjon.backend.utils.readers.FavoriteHandler;
+import se.chalmers.student.devit.resekompanjon.fragment.NavigationDrawerFragment;
 
 /**
  * @author Jonathan
  * @version 0.1
  */
-public class FavoritesActivity extends AppCompatActivity {
+public class FavoritesActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
+    private NavigationDrawerFragment mNavigationDrawerFragment;
+    private FavoriteArrayAdapter adapter;
+    private ListView listView;
+    private ArrayList<JsonObject> list;
 
     public FavoritesActivity() {}
 
@@ -15,8 +35,44 @@ public class FavoritesActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.favorite_layout);
+        FavoriteHandler handler = new FavoriteHandler(this);
+        JsonArray jsonArray = handler.getTripArrayAsJson();
+        list = new ArrayList<>();
+        for(int i=0; i<jsonArray.size(); i++) {
+            JsonObject obj = jsonArray.get(i).getAsJsonObject();
+            list.add(obj);
+        }
 
+        setContentView(R.layout.favorite_layout);
+        listView = (ListView) findViewById(R.id.list);
+        adapter = new FavoriteArrayAdapter(this, list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
+
+        mNavigationDrawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+        // Set up the drawer.
+        mNavigationDrawerFragment.setUp(
+                R.id.navigation_drawer,
+                (DrawerLayout) findViewById(R.id.favorite_trip_drawer_layout));
+    }
+
+    /**
+     * Callback method to be invoked when an item in this AdapterView has
+     * been clicked.
+     * <p/>
+     * Implementers can call getItemAtPosition(position) if they need
+     * to access the data associated with the selected item.
+     *
+     * @param parent   The AdapterView where the click happened.
+     * @param view     The view within the AdapterView that was clicked (this
+     *                 will be a view provided by the adapter)
+     * @param position The position of the view in the adapter.
+     * @param id       The row id of the item that was clicked.
+     */
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
