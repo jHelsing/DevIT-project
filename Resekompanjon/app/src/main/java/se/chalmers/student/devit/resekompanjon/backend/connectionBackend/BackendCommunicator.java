25ef760fd8ede6cc7853442fa691e3011687af0a
Marcus Aurén <auren@student.chalmers.se>
2015-10-20 12:29:04 +0200

    Added if to fix everything I broke

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/backend/connectionBackend/BackendCommunicator.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/backend/connectionBackend/BackendCommunicator.java
@@ -140,9 +140,11 @@ public class BackendCommunicator implements OnTaskCompleted{
     public JsonElement getApiData() throws NoJsonAvailableException, NoTripFoundException {
         if (apiData == null){
             throw new NoJsonAvailableException();
-        } else if (apiData.getAsJsonObject().get("Fail").getAsString().equals("No connection found")){
-            Log.d("Throwing", "adw");
-            throw new NoTripFoundException();
+        } else if (apiData.getAsJsonObject().has("Fail")){
+            if (apiData.getAsJsonObject().get("Fail").getAsString().equals("No connection found")) {
+                Log.d("Throwing", "adw");
+                throw new NoTripFoundException();
+            }
         }
         return apiData;
     }
