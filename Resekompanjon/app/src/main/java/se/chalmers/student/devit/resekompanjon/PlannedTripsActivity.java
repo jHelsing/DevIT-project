daf277deb92ebf233a863cb961ec8e37fe600b1a
Amaarillo <Amar_kulaglic@hotmail.com>
2015-10-27 19:57:57 +0100

    Now user know if there is planned trips or not

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/PlannedTripsActivity.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/PlannedTripsActivity.java
@@ -1,5 +1,6 @@
 package se.chalmers.student.devit.resekompanjon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
@@ -68,15 +70,34 @@ public class PlannedTripsActivity extends AppCompatActivity implements Navigatio
             });*/
         } else {
             //There are no planned trips available
+            Toast noConectionMessage = Toast.makeText(this
+                    , "Tyvärr, men du måste lägga till en"
+                    + " resa som planerad resa för att kunna gå vidare.", Toast.LENGTH_LONG);
+            noConectionMessage.show();
+            Thread thread = new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(3500);
+                        Intent i = new Intent(PlannedTripsActivity.this, ResekompanjonActivity.class);
+                        startActivity(i);
+                        finish();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            thread.start();
         }
 
         // Setting navigation
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
+        /** mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.planned_trip_drawer_layout));
+         **/
     }
 
     /**
