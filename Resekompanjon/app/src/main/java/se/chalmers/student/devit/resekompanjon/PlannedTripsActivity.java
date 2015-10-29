17c54b20397f98a0c0ce50128d4aa5e039420647
Marcus Aurén <auren@student.chalmers.se>
2015-10-29 13:06:20 +0100

    nav fragment now creates as intended

--- Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/PlannedTripsActivity.java
+++ Resekompanjon/app/src/main/java/se/chalmers/student/devit/resekompanjon/PlannedTripsActivity.java
@@ -2,6 +2,7 @@ package se.chalmers.student.devit.resekompanjon;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
@@ -41,9 +42,18 @@ public class PlannedTripsActivity extends AppCompatActivity implements Navigatio
                 arrayList.add(obj);
             }
             setContentView(R.layout.planned_trip_layout);
+            mNavigationDrawerFragment = (NavigationDrawerFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+            // Set up the drawer.
+            mNavigationDrawerFragment.setUp(
+                    R.id.navigation_drawer,
+                    (DrawerLayout) findViewById(R.id.planned_trip_drawer_layout));
+            
             expListView = (ExpandableListView) findViewById(R.id.expandableListView);
             adapter = new PlannedTripExpandableListAdapter(this, arrayList);
             expListView.setAdapter(adapter);
+
+
         } else {
             //There are no planned trips available
             Toast noConectionMessage = Toast.makeText(this
